fac7127b4f1e3786761e87478fd8cd5724fd3732
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2025-09-20 14:47:02 -0700

    clean up session

--- handlers/logout.go
+++ handlers/logout.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 
-	session2 "github.com/lthummus/auththingie2/middlewares/session"
+	"github.com/lthummus/auththingie2/middlewares/session"
 )
 
 func (e *Env) HandleLogout(w http.ResponseWriter, r *http.Request) {
@@ -14,14 +14,14 @@ func (e *Env) HandleLogout(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sess, err := session2.NewDefaultSession()
+	sess, err := session.NewDefaultSession()
 	if err != nil {
 		log.Warn().Err(err).Msg("could not create default session")
 		http.Error(w, "could not create default session", http.StatusInternalServerError)
 		return
 	}
 
-	err = session2.WriteSession(w, r, sess)
+	err = session.WriteSession(w, r, sess)
 	if err != nil {
 		log.Warn().Err(err).Msg("could not sign user out")
 		http.Error(w, "could not sign user out", http.StatusInternalServerError)
