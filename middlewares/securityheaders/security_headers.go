d1aa8b2d3e8ee4c190a50583604a5f70d58e4fd0
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2025-10-14 08:44:43 -0700

    remove CSP until i can figure out what i need to set for HTMX

--- middlewares/securityheaders/security_headers.go
+++ middlewares/securityheaders/security_headers.go
@@ -16,7 +16,6 @@ func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Frame-Options", "DENY")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
-	w.Header().Set("Content-Security-Policy", "default-src 'self'; img-src 'self' data:;")
 
 	m.handler.ServeHTTP(w, r)
 }
