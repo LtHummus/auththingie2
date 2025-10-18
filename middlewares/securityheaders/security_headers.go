c1a815d05c574c2002e2683144ae75ca708d4692
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2025-10-17 21:50:18 -0700

    replace all onclicks

--- middlewares/securityheaders/security_headers.go
+++ middlewares/securityheaders/security_headers.go
@@ -16,6 +16,7 @@ func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Frame-Options", "DENY")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+	w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;")
 
 	m.handler.ServeHTTP(w, r)
 }
